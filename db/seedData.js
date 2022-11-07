07d1ddb1cc96ee86dbac6c5b530bc4e1bf3b4457
Leonardo <cruzleonardo831@gmail.com>
2022-11-07 09:48:39 -0600

    first steps

--- db/seedData.js
+++ db/seedData.js
@@ -3,12 +3,56 @@
 const client = require("./client")
 
 async function dropTables() {
-  console.log("Dropping All Tables...")
+  try{
+    await client.query(`DROP TABLE IF EXISTS users;`);
+    await client.query(`DROP TABLE IF EXISTS activities;`);
+    await client.query(`DROP TABLE IF EXISTS routine;`);
+    await client.query(`DROP TABLE IF EXISTS routineActivities;`);
+    console.log("Dropping All Tables...")
+  }catch(error){
+    throw error;
   // drop all tables, in the correct order
 }
 
 async function createTables() {
   console.log("Starting to build tables...")
+  try{
+    await client.query(`
+    CREATE TABLE users(
+      id SERIAL PRIMARY KEY,
+      username VARCHAR(255) UNIQUE NOT NULL,
+      password VARCHAR(255) NOT NULL
+    );
+    `)
+    await client.query(`
+    CREATE TABLE activities(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(255) UNIQUE NOT NULL,
+      description TEXT NOT NULL
+    );
+    `)
+    await client.query(`
+    CREATE TABLE routines(
+      id SERIAL PRIMARY KEY,
+      "creatorId" INTEGER REFERENCES users(id),
+      "isPublic" BOOLEAN DEFAULT false,
+      name VARCHAR(255) UNIQUE NOT NULL,
+      goal TEXT NOT NULL
+    );
+    `)
+    await client.query(`
+    CREATE TABLE routineActivities(
+      id SERIAL PRIMARY KEY,
+      "routineId" INTEGER REFERENCES routines(id),
+      "activityId" REFERENCES activities(id),
+      duration INTEGER,
+      count INTEGER
+    );
+    `)
+
+  }catch(error){
+    throw error
+  }
   // create all tables, in the correct order
 }
 
