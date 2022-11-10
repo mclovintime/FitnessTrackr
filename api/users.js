fa7875e251aff4c9f672dce8e11921af6545c3d5
Leonardo <cruzleonardo831@gmail.com>
2022-11-10 14:33:31 -0600

    create user starting to work

--- api/users.js
+++ api/users.js
@@ -1,47 +1,78 @@
+/* eslint-disable no-useless-catch */
 const express = require('express');
 const { getUserByUsername, createUser } = require('../db');
 const router = express.Router();
 const jwt = require("jsonwebtoken");
-
+router.use((req, res, next)=>{
+    console.log("A request has been made to /users");
+    next();
+})
 // POST /api/users/login
 
 
 // POST /api/users/register
 
-router.post("/api/users/register", async (req, res, next) =>  {
-    console.log("this is a big ol test")
-    const   {username, password} = req.body;
-    try {
-const user = await getUserByUsername(username);
 
-if (password.length < 8)    {
-    next({
-        message: "Password must be 8 characters or longer",
-        name: "Password error"
-    });
-}
+router.post('/register', async (req, res, next) => {
+    const{username, password}= req.body;
 
-if (user)   {
-    next({
-        name: "UserExistsError",
-        message: "This user already exists",
-    });
-}
-
-const newUser = await createUser({
-    username,
-    password
-});
-console.log(newUser, "this is new user")
-
-res.send({newUser})
-
-return newUser;
-
-    } catch (error){
-throw error
+    try{
+        const userObj = await getUserByUsername(username)
+        if (userObj){
+            next({
+                name:'UserExistsError',
+                message:'A user by that username already exists'
+            })
+        }
+        const newUser = await createUser({
+            username: username, 
+            password: password, 
+        })
+        const token = jwt.sign(
+            newUser, 
+            process.env.JWT_SECRET, 
+        )
+        res.send({
+            message: "thank you for signing up",
+            token: token,
+            user: {id:newUser.id,
+                 username: newUser.username},
+         
+        })
+    }catch ({name, message}) {
+        next({name, message})
     }
+
 })
+
+//     if (password.length < 8){
+//     next({
+//         name: "Password error",
+//         message: "Password must be 8 characters or longer"
+//     });
+// }
+
+// if (user)   {
+//     next({
+//         name: "UserExistsError",
+//         message: "This user already exists",
+//     });
+// }
+
+// const newUser = await createUser({
+//     username,
+//     password
+// });
+// console.log(newUser, "this is new user")
+
+// res.send({newUser})
+
+// return newUser;
+
+//     } catch (error){
+// throw error
+//     }
+
 
 // GET /api/users/me
 
--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 require("dotenv").config()
 const express = require("express")
 const app = express()
+const cors = require("cors")
+app.use(cors())
+
+const morgan = require('morgan');
+app.use(morgan('dev'));
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log('<___Body Logger START___>');
+    console.log(req.body);
+    console.log('<___Body Logger END___>');
+    
+    next();
+})
+
+const apiRouter = require("./api");
+app.use('/api', apiRouter);
+
+const client = require('./db/client');
+client.connect();
 
 // Setup your Middleware and API Router here
 
