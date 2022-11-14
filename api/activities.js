44d28ce982f8c6addd006397795d6e0c231caa62
Leonardo <cruzleonardo831@gmail.com>
2022-11-13 23:04:23 -0600

    issues

--- api/activities.js
+++ api/activities.js
@@ -1,26 +1,39 @@
 const express = require('express');
-const {getAllActivities} = require("../db")
+const { request } = require('../app');
+const {getAllActivities} = require("../db");
+const { UserDoesNotExistError } = require('../errors');
 const activitiesRouter = express.Router();
 
 
 // GET /api/activities/:activityId/routines
+activitiesRouter.get('/:activityId/routines', async (req, res, next)=>{
+    const activityId = req.params.activityId
+    console.log('activity Id', activityId)
+    try{
+        const returnedRoutines = await getAllActivities(activityId);
+        res.send({
+            activities: returnedRoutines
+        })
+    }catch ({name, message}){
+        next({name, message})
+    }
+})
 
 
 // GET /api/activities
-activitiesRouter.get('/activities', async (req, res, next) => {
-    
-   
+activitiesRouter.get('/', async (req, res, next) => {
+    const allActivities = req.params.allActivities
+    /////////// THIS ONE IS BASICALLY DONE, BUT I'M NOT SURE HOW TO RETURN JUST THE ARRAY WITH THE OBJECT, CHECK THE TEST OUTPUT, ITS ALMOST IDENTICAL//////////////
     try{
-        const allActivities = await getAllActivities()
-        console.log(allActivities, "all activities")
-res.send(allActivities)
-
-
-    }catch ({name, message}) {
+        const returnedActivities = await getAllActivities(allActivities);
+        res.send({
+            returnedActivities
+        })
+    }catch ({name, message}){
         next({name, message})
     }
-
 })
+       
 
 // POST /api/activities
 
--- api/routineActivities.js
+++ api/routineActivities.js
@@ -1,8 +1,59 @@
 const express = require('express');
+const { getActivityById, updateActivity, updateRoutine, deleteActivity } = require('../db');
 const router = express.Router();
 
 // PATCH /api/routine_activities/:routineActivityId
 
+// router.patch('/:activityId', express, async (req, res, next) => {
+//     const activity_Id = req.params.activityId;
+//     const {activityName, description}=req.body;
+//     const fields={}
+//     if (activityName){
+//         fields.activityName=activityName
+//     }
+//     if (description){
+//         fields.description=description
+//     }
+
+//     try{
+//         const originalActivity = await getActivityById(activity_Id);
+//         if(originalActivity.author.id===req.user.id){
+//             const updateActivity = await updateActivity(activity_Id, fields);
+//             res.send({activity: updateActivity})
+//         }else{
+//             next({
+//                 name: 'UnauthorizedUserError',
+//                 message: 'You cannot update a post that is not yours'
+//             })
+//         }
+//     }catch({name, message}){
+//         next({name, message})
+//     }console.log(updateRoutine, "updated routine")
+// })
+
+
 // DELETE /api/routine_activities/:routineActivityId
+
+router.delete('/:activityId', express, async(req, res, next)=>{
+    const activity_Id=req.params.activityId
+    console.log(activity_Id, "i am activity id");
+
+    try{
+        const activityToDelete = await getActivityById(activity_Id)
+
+        if(activityToDelete.creator.id===req.user.id){
+            const returnedActivity = await deleteActivity(activity_Id)
+            console.log(returnedActivity, "i am returned activity")
+            res.send({"activity": returnedActivity})
+        }else{
+            next({
+                name: 'UnauthorizedUserError',
+                message: 'You cannot delete a post that is not yours'
+            })
+        }
+    }catch({name, message}){
+        next({name, message})
+    }
+})
 
 module.exports = router;
