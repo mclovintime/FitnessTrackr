627d550630057ab507007f98ebf3a308695332c0
Thomas McLaughlin <123tommygun1@gmail.com>
2022-11-14 10:27:43 -0500

    getting through activities

--- api/activities.js
+++ api/activities.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const { request } = require('../app');
-const {getAllActivities} = require("../db");
-const { UserDoesNotExistError } = require('../errors');
+const {getAllActivities, getActivityById, attachActivitiesToRoutines, createActivity, getActivityByName} = require("../db");
 const activitiesRouter = express.Router();
 
 
 // GET /api/activities/:activityId/routines
 activitiesRouter.get('/:activityId/routines', async (req, res, next)=>{
+    
     const activityId = req.params.activityId
     console.log('activity Id', activityId)
     try{
-        const returnedRoutines = await getAllActivities(activityId);
+        const returnedRoutines = await getActivityById(activityId);
+
         res.send({
             activities: returnedRoutines
         })
@@ -26,9 +26,11 @@ activitiesRouter.get('/', async (req, res, next) => {
     /////////// THIS ONE IS BASICALLY DONE, BUT I'M NOT SURE HOW TO RETURN JUST THE ARRAY WITH THE OBJECT, CHECK THE TEST OUTPUT, ITS ALMOST IDENTICAL//////////////
     try{
         const returnedActivities = await getAllActivities(allActivities);
-        res.send({
+        
+       
+        res.send(
             returnedActivities
-        })
+    )
     }catch ({name, message}){
         next({name, message})
     }
@@ -36,6 +38,32 @@ activitiesRouter.get('/', async (req, res, next) => {
        
 
 // POST /api/activities
+activitiesRouter.post('/', async (req, res, next) => {
+    try{
+       const{name, description} = req.body
+       const returnedName = await getActivityByName(name)
+       if (returnedName.name == name)   {
+        res.send({
+            error: "This is all wrong",
+            message: `An activity with name ${name} already exists`,
+            name: "This is all wrong"
+        })
+       }
+       
+       if (returnedName.name !== name)   {
+       const newActivity = await createActivity(name, description)
+       console.log(name, description, "abcd")
+       console.log(newActivity, "efg")
+       
+        res.send(
+            newActivity
+    )
+        }
+        
+    }catch ({name, message}){
+        next({name, message})
+    }
+})
 
 // PATCH /api/activities/:activityId
 
