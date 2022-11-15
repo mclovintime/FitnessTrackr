d7885b15814cadfb6440525bc52d99586f8569f3
Leonardo <cruzleonardo831@gmail.com>
2022-11-14 23:17:45 -0600

    Started working on routines

--- api/routineActivities.js
+++ api/routineActivities.js
@@ -1,42 +1,17 @@
 const express = require('express');
-const { getActivityById, updateActivity, updateRoutine, deleteActivity } = require('../db');
+const { getActivityById, updateActivity, updateRoutine, deleteActivity, getRoutineById } = require('../db');
 const router = express.Router();
 
 // PATCH /api/routine_activities/:routineActivityId
 
-// router.patch('/:activityId', express, async (req, res, next) => {
-//     const activity_Id = req.params.activityId;
-//     const {activityName, description}=req.body;
-//     const fields={}
-//     if (activityName){
-//         fields.activityName=activityName
-//     }
-//     if (description){
-//         fields.description=description
-//     }
-
-//     try{
-//         const originalActivity = await getActivityById(activity_Id);
-//         if(originalActivity.author.id===req.user.id){
-//             const updateActivity = await updateActivity(activity_Id, fields);
-//             res.send({activity: updateActivity})
-//         }else{
-//             next({
-//                 name: 'UnauthorizedUserError',
-//                 message: 'You cannot update a post that is not yours'
-//             })
-//         }
-//     }catch({name, message}){
-//         next({name, message})
-//     }console.log(updateRoutine, "updated routine")
-// })
+router.patch('/:routineId')
 
 
 // DELETE /api/routine_activities/:routineActivityId
 
 router.delete('/:activityId', express, async(req, res, next)=>{
     const activity_Id=req.params.activityId
-    console.log(activity_Id, "i am activity id");
+    // console.log(activity_Id, "i am activity id");
 
     try{
         const activityToDelete = await getActivityById(activity_Id)
--- api/routines.js
+++ api/routines.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const { getAllRoutines, createRoutine } = require('../db');
+const { checkToken } = require('./utils');
 const router = express.Router();
 
 // GET /api/routines
 
+router.get('/', async(req, res)=>{
+    const routines = await getAllRoutines()
+    res.send(routines)
+})
 // POST /api/routines
+router.post('./:routines', async (req, res, next)=>{
+    try{
+        const{name} = req.body
+        const newActivity = await createRoutine(name)
+        if (newActivity){
+            if(newActivity.name == name){
+                next({
+                    error: "error",
+                    message: `A routine with name ${name} already exists`
+                })
+            }
+        }
+    }catch (error){
+        console.error(error)
+    }
+})
 
 // PATCH /api/routines/:routineId
 
